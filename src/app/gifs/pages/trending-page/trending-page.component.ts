1d9945e466bd1fe8055cdefa9e77af9d8e1d150b
Eduardo Garcia Camacho <heduardo.garcia@gmail.com>
2025-06-02 23:36:18 -0700

    Version final trending con scroll y manteniendo localstorga

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,29 +1,44 @@
-import {  Component, inject, signal } from '@angular/core';
-import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import {  AfterViewInit, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifsService } from '../../services/gifs.service';
+import { ScrollStateService } from '../../../shared/services/scroll-state.services';
 
-// const imageUrls: string[] = [
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-//   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-// ];
+
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifListComponent],
+  // imports: [],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+    scrollDiv.scrollTop = this.ScrollStateService.trendingScrollState();
+  }
   gifsService=inject(GifsService);
+  ScrollStateService=inject(ScrollStateService);
+  scrollDivRef= viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+  onScroll(event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+    // console.log('scrollTop', scrollTop);
+    // console.log('clientHeight', clientHeight);
+    // console.log('scrollDiv', scrollHeight);
+    // console.log(scrollTop + clientHeight +300>= scrollHeight )
+    const isAtBottom=scrollTop + clientHeight +300>= scrollHeight ;
+    // console.log('isAtBottom', isAtBottom);
+    this.ScrollStateService.trendingScrollState.set(scrollTop);
+    if (isAtBottom) {
+       this.gifsService.loadTrendingGifs();
+    } else {
+      console.log('Not enough scroll to load more gifs');
+    }
 
+
+
+  }
   // gifs=signal(imageUrls);
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,7 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map, Observable, tap } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 import { O } from 'node_modules/@angular/router/router_module.d-6zbCxc1T';
 
 const GIF_KEY='gifs';
@@ -24,7 +24,18 @@ export class GifsService {
   private http=inject(HttpClient);
 
   trendinGifs=signal<Gif[]>([]);
-  trendingGifLoading=signal(true);
+  trendingGifLoading=signal(false);
+  private trendingPage=signal(0);
+
+  trendingGifGroup=computed<Gif[][]>(()=>{
+    const groups=[];
+    for(let i=0;i<this.trendinGifs().length;i+=3){
+      groups.push(this.trendinGifs().slice(i,i+3));
+    }
+    console.log({groups});
+
+    return groups;
+  })
 
   searchHistory=signal<Record<string,Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys=computed(()=>Object.keys(this.searchHistory()));
@@ -39,10 +50,13 @@ export class GifsService {
   // This method is used to load trending gifs from the Giphy API
 
   loadTrendingGifs(){
+    if(this.trendingGifLoading()) return;
+    this.trendingGifLoading.set(true);
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`,{params:{api_key:environment.giphyApiKey,limit:20,rating:'g'}})
     .subscribe((response:any)=>{
       const gifs=GifMapper.mapGiphyItemsToGifArray(response.data);
-      this.trendinGifs.set(gifs);
+      this.trendinGifs.update(currentGifs=>[...currentGifs, ...gifs]);
+      this.trendingPage.update((page)=>page+1);
       this.trendingGifLoading.set(false);
       console.log(gifs);
     }
@@ -50,7 +64,16 @@ export class GifsService {
 
   }
   searchGifs(query:string):Observable<Gif[]>{
-    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`,{params:{api_key:environment.giphyApiKey,limit:20,rating:'g',q:query}})
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`,
+        {params:
+          {
+            api_key:environment.giphyApiKey,
+            limit:20,
+            offset:this.trendingPage()*20,
+            rating:'g',
+
+            q:query}})
     .pipe(
       // tap(resp=>console.log({resp}))
       // map((resp)=>console.log(`Hola mundo ${resp.data.length}`)),
